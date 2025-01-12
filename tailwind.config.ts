d8294969f727960370c770a77ea681b9e74218b2
asifulhaque087 <asifulhaque087@gmail.com>
2025-01-12 17:49:05 +0600

    Change primary color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -21,7 +21,8 @@ export default {
   				foreground: 'hsl(var(--popover-foreground))'
   			},
   			primary: {
-  				DEFAULT: 'hsl(var(--primary))',
+  				// DEFAULT: 'hsl(var(--primary))',
+  				DEFAULT: '#ed1e61',
   				foreground: 'hsl(var(--primary-foreground))'
   			},
   			secondary: {
